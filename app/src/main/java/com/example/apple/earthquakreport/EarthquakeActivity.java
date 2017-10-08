b9fd50354f157d13877f3de113079c57b4ffcb0b
vinit <vinitkantrod@rediffmail.com>
2017-10-08 16:10:12 +0530

    loader class implemented with emptyView

--- app/src/main/java/com/example/apple/earthquakreport/EarthquakeActivity.java
+++ app/src/main/java/com/example/apple/earthquakreport/EarthquakeActivity.java
@@ -1,48 +1,46 @@
 package com.example.apple.earthquakreport;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.attr.data;
-import static android.os.Build.VERSION_CODES.O;
+import static android.icu.lang.UCharacter.JoiningGroup.E;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getSimpleName();
 
     private static final String DATA_USGS = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=1&limit=50";
-
+    private TextView mEmptyStateTextView;
     private EarthquakeAdapter mAdapter;
+    private static final int EARTHQUAKE_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
 
-// Find a reference to the {@link ListView} in the layout
+        // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
+        earthquakeListView.setEmptyView(mEmptyStateTextView);
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
@@ -69,58 +67,35 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
-        // Start the AsyncTask to fetch the earthquake data
-        DownloadData task = new DownloadData();
-        task.execute(DATA_USGS);
-
+        LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
     }
 
-    public void updateUI(ArrayList<Earthquake> earthquakes) {
-
-        ListView listView =  (ListView) findViewById(R.id.list);
-        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Earthquake currentEarthquake = adapter.getItem(i);
-                if (currentEarthquake != null) {
-                    if (currentEarthquake.getUrl() != null) {
-                        Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
-                        Intent newIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
-                        startActivity(newIntent);
-                    } else {
-                        Log.i(LOG_TAG, "No URL specified for current listItem" + i);
-                    }
-                }
-
-            }
-        });
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new EarthquakeLoader(this, DATA_USGS);
     }
 
-    private class DownloadData extends AsyncTask<String, Void, List<Earthquake>> {
 
-        @Override
-        protected List<Earthquake> doInBackground(String... urls) {
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
+        // Clear the adapter of previous earthquake data
+        mEmptyStateTextView.setText(R.string.no_earthquake);
+        mAdapter.clear();
 
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-
-            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
-            return result;
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (earthquakes != null && !earthquakes.isEmpty()) {
+            mAdapter.addAll(earthquakes);
         }
+    }
 
-        @Override
-        protected void onPostExecute(List<Earthquake> data) {
-            mAdapter.clear();
-            if (data != null && !data.isEmpty()) {
-                mAdapter.addAll(data);
-            }
-//            updateUI(data);
-        }
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 
 }
